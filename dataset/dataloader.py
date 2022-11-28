c2942b4463367d43a0d83ab814cc1afe0e6f030e
Vũ Đức Mạnh <manh.vu1407@hcmut.edu.vn>
2022-11-28 20:16:26 +0700

    change preprocess and dataloader

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -9,6 +9,8 @@ import torch
 import random
 import pandas
 import warnings
+from torchvision import transforms
+from PIL import Image
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -25,7 +27,7 @@ sys.path.append("..")
 class BaseFeeder(data.Dataset):
     # initialization
     def __init__(self, folder_path, prefix="./", mean=None, std=None, mode='train', resize_h=224,
-    resize_w=224, img_dim=1):
+    resize_w=224, img_dim=1, data_type="folder"):
         self.folder_path = folder_path
         self.prefix = prefix
         self.files = []
@@ -33,15 +35,23 @@ class BaseFeeder(data.Dataset):
         self.resize_w=resize_w
         self.img_dim=img_dim
         self.mode = mode
+        self.data_type = data_type
         folder = os.path.join(prefix, folder_path, mode, "")
-        print(folder)
-        for file in os.listdir(folder):
-            d = os.path.join(folder, file)
-            if os.path.isdir(d):
-                if os.path.isfile(d + "/{}_0".format(file) + ".png"):
-                    self.files.append(d + "/{}_0".format(file))
-                if os.path.isfile(d + "/{}_1".format(file) + ".png"):
-                    self.files.append(d + "/{}_1".format(file))
+        if data_type == "folder":
+            for file in os.listdir(folder):
+                d = os.path.join(folder, file)
+                if os.path.isdir(d):
+                    if os.path.isfile(d + "/{}_0".format(file) + ".png"):
+                        self.files.append(d + "/{}_0".format(file))
+                    if os.path.isfile(d + "/{}_1".format(file) + ".png"):
+                        self.files.append(d + "/{}_1".format(file))
+        else:
+            for file in os.listdir(folder):
+                d = os.path.join(folder, file)
+                if os.path.isfile(d):
+                    if os.path.isfile(d):
+                        self.files.append(d)
+         
         
         if mean == None or std == None:
             print("Calculating mean and std of the {} set...".format(mode))
@@ -57,18 +67,15 @@ class BaseFeeder(data.Dataset):
         for file in self.files:
             num_image+=1
 
-            img_path = file + ".png"
-            
-            image = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
+            img_path = file + ".png" if self.data_type == "folder" else file        
+            image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+
             image = torch.from_numpy(cv2.resize(image, (self.resize_h,
             self.resize_w), interpolation = cv2.INTER_AREA)).type(torch.FloatTensor)
             
-            if self.img_dim == 1:
-                psum    += image.sum()
-                psum_sq += (image ** 2).sum()
-            else:
-                psum    += image.sum()
-                psum_sq += (image ** 2).sum() 
+
+            psum    += image.sum()
+            psum_sq += (image ** 2).sum()
 
         count = num_image*self.resize_h*self.resize_w                      
         total_mean = psum / count
@@ -76,19 +83,35 @@ class BaseFeeder(data.Dataset):
         total_std  = torch.sqrt(total_var)
 
         return total_mean.item(), total_std.item()
+
+
     # getitem attribute
     def __getitem__(self, idx):
-        img_path = self.files[idx] + ".png"
-        image = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
-        image = cv2.resize(image, (224,224), interpolation = cv2.INTER_AREA)
-        image = np.expand_dims(image, axis=0)
-        image = torch.from_numpy(image).type(torch.FloatTensor)
+        img_path = self.files[idx] + ".png" if self.data_type == "folder" else self.files[idx]       
+        image = np.asarray(Image.open(img_path))
+        print(image.shape)
+        # resize image
+        # to add data augmentation
+        image = torch.from_numpy(cv2.resize(image, (self.resize_h,
+        self.resize_w), interpolation = cv2.INTER_AREA)).type(torch.FloatTensor)
+
+        # to 3d tensor
+        image = torch.unsqueeze(image, 0)
+
+        # copy and concatenate
+        image  = image.repeat(3, 1, 1)
+        # normalize
         image = (image - self.mean)/(self.std + 0.00001)
 
+        
         mask_path = self.files[idx] + "_cancer.png"
-        mask = cv2.imread(mask_path, cv2.IMREAD_ANYDEPTH)
-        mask = cv2.resize(mask, (224,224), interpolation = cv2.INTER_AREA)        
+        mask = np.asarray(Image.open(mask_path))
+        max_px = np.amax(mask)
+        if max_px > 0:
+            mask = mask/max_px
+        mask = cv2.resize(mask, (self.resize_w, self.resize_h), interpolation = cv2.INTER_AREA)        
         mask = torch.from_numpy(mask).type(torch.FloatTensor)
+        
         mask = torch.unsqueeze(mask, 0)
 
         return image, mask
@@ -120,9 +143,13 @@ class BaseFeeder(data.Dataset):
 
 if __name__ == "__main__":
 
-    pass
-
-    # feeder = BaseFeeder(r"E:\CSAWS\CSAW-S\CsawS\anonymized_dataset", prefix="")
+    img = cv2.imread(r"D:\breast_cancer_classification\00002_20990909_L_CC_1.png", cv2.IMREAD_UNCHANGED)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    print(img.shape)
+    img2 = Image.open(r"D:\breast_cancer_classification\00002_20990909_L_CC_1.png")
+    img2 = np.asarray(img2)
+    print(img2.shape)
+    # feeder = BaseFeeder(r"E:\CSAWS\CSAW-S\CsawS\\", prefix="",img_dim=1, data_type="folder", mean=3, std=1)
     # dataloader = torch.utils.data.DataLoader(
     #     dataset=feeder,
     #     batch_size=2,
--- preprocess/dataset_preprocess.py
+++ preprocess/dataset_preprocess.py
@@ -3,7 +3,7 @@ import os
 import cv2
 import pdb
 import glob
-import pandas
+import pandas as pd
 import argparse
 import numpy as np
 from tqdm import tqdm
@@ -11,69 +11,37 @@ from functools import partial
 from multiprocessing import Pool
 
 
-def csv2dict(anno_path, dataset_type):
-    inputs_list = pandas.read_csv(anno_path)
-    if dataset_type == 'train':
-        broken_data = [2390]
-        inputs_list.drop(broken_data, inplace=True)
-    inputs_list = (inputs_list.to_dict()['id|folder|signer|annotation'].values())
-    info_dict = dict()
-    info_dict['prefix'] = anno_path.rsplit("/", 3)[0] + "/features/fullFrame-210x260px"
-    print(f"Generate information dict from {anno_path}")
-    for file_idx, file_info in tqdm(enumerate(inputs_list), total=len(inputs_list)):
-        fileid, folder, signer, label = file_info.split("|")
-        num_frames = len(glob.glob(f"{info_dict['prefix']}/{dataset_type}/{folder}"))
-        info_dict[file_idx] = {
-            'fileid': fileid,
-            'folder': f"{dataset_type}/{folder}",
-            'signer': signer,
-            'label': label,
-            'num_frames': num_frames,
-            'original_info': file_info,
-        }
-    return info_dict
+def csawcc_preprocess(image_folder, mask_folder, save_folder=None, mask_postfix="mask", train_ratio=0.9):
+    """
+    Create a metadata file for CSAW_CC dataset
+    """
 
+    df = pd.DataFrame(columns=['patient_id', 'date', 'laterality', 'view', 'num', 'data_path','mask_path','mode'], )
+    num_of_train_data = int(train_ratio*len(os.listdir(image_folder)))
 
-def generate_gt_stm(info, save_path):
-    with open(save_path, "w") as f:
-        for k, v in info.items():
-            if not isinstance(k, int):
-                continue
-            f.writelines(f"{v['fileid']} 1 {v['signer']} 0.0 1.79769e+308 {v['label']}\n")
+    for i, file in enumerate(os.listdir(image_folder)):
+        d = os.path.join(image_folder, file)
+        if os.path.isfile(d):
+            data_path = os.path.abspath(d)
+            patient_info = file.replace(".png","").split("_")
+            mask_path = os.path.join(mask_folder, "_".join(patient_info + [mask_postfix]) + ".png")
+            
+            if not os.path.isfile(mask_path):
+                print("Cannot find mask of {} file!!!".format(file))
+                break
+            
+            mode = "train" if i < num_of_train_data else "test"
+            df = pd.concat([pd.DataFrame([patient_info + [data_path, mask_path, mode]], columns=df.columns), df], ignore_index=True)
+    
 
+    if save_folder == None:
+        csv_file = os.path.join(os.getcwd,"preprocess","metadata.csv")
+    else:
+        csv_file = os.path.join(save_folder,"metadata.csv")
 
-def sign_dict_update(total_dict, info):
-    for k, v in info.items():
-        if not isinstance(k, int):
-            continue
-        split_label = v['label'].split()
-        for gloss in split_label:
-            if gloss not in total_dict.keys():
-                total_dict[gloss] = 1
-            else:
-                total_dict[gloss] += 1
-    return total_dict
+    df.to_csv(csv_file,index_label="id")
+            
 
-
-def resize_img(img_path, dsize='210x260px'):
-    dsize = tuple(int(res) for res in re.findall("\d+", dsize))
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, dsize, interpolation=cv2.INTER_LANCZOS4)
-    return img
-
-
-def resize_dataset(video_idx, dsize, info_dict):
-    info = info_dict[video_idx]
-    img_list = glob.glob(f"{info_dict['prefix']}/{info['folder']}")
-    for img_path in img_list:
-        rs_img = resize_img(img_path, dsize=dsize)
-        rs_img_path = img_path.replace("210x260px", dsize)
-        rs_img_dir = os.path.dirname(rs_img_path)
-        if not os.path.exists(rs_img_dir):
-            os.makedirs(rs_img_dir)
-            cv2.imwrite(rs_img_path, rs_img)
-        else:
-            cv2.imwrite(rs_img_path, rs_img)
 
 
 def run_mp_cmd(processes, process_func, process_args):
@@ -87,11 +55,22 @@ def run_cmd(func, args):
 
 
 if __name__ == '__main__':
+    
+    csawcc_preprocess(
+        image_folder=r"E:\csawcc_png16\images_and_masks\images_and_masks\images\png16",
+        mask_folder=r"E:\csawcc_png16\images_and_masks\images_and_masks\masks",
+        save_folder=r"D:\framework\\breast_cancer_segmentation\preprocess"
+    )
+
+    df = pd.read_csv(r'D:\framework\\breast_cancer_segmentation\preprocess\metadata.csv')
+
+    pass
+
     parser = argparse.ArgumentParser(
-        description='Data process for Visual Alignment Constraint for Continuous Sign Language Recognition.')
-    parser.add_argument('--dataset', type=str, default='phoenix2014',
+        description='Data process for .')
+    parser.add_argument('--dataset', type=str, default='none',
                         help='save prefix')
-    parser.add_argument('--dataset-root', type=str, default='../dataset/phoenix2014/phoenix-2014-multisigner',
+    parser.add_argument('--dataset-root', type=str, default='../none',
                         help='path to the dataset')
     parser.add_argument('--annotation-prefix', type=str, default='annotations/manual/{}.corpus.csv',
                         help='annotation prefix')
@@ -103,29 +82,4 @@ if __name__ == '__main__':
                         help='whether adopts multiprocessing to accelate the preprocess')
 
     args = parser.parse_args()
-    mode = ["dev", "test", "train"]
-    sign_dict = dict()
-    if not os.path.exists(f"./{args.dataset}"):
-        os.makedirs(f"./{args.dataset}")
-    for md in mode:
-        # generate information dict
-        information = csv2dict(f"{args.dataset_root}/{args.annotation_prefix.format(md)}", dataset_type=md)
-        np.save(f"./{args.dataset}/{md}_info.npy", information)
-        # update the total gloss dict
-        sign_dict_update(sign_dict, information)
-        # generate groudtruth stm for evaluation
-        generate_gt_stm(information, f"./{args.dataset}/{args.dataset}-groundtruth-{md}.stm")
-        # resize images
-        video_index = np.arange(len(information) - 1)
-        print(f"Resize image to {args.output_res}")
-        if args.process_image:
-            if args.multiprocessing:
-                run_mp_cmd(10, partial(resize_dataset, dsize=args.output_res, info_dict=information), video_index)
-            else:
-                for idx in tqdm(video_index):
-                    run_cmd(partial(resize_dataset, dsize=args.output_res, info_dict=information), idx)
-    sign_dict = sorted(sign_dict.items(), key=lambda d: d[0])
-    save_dict = {}
-    for idx, (key, value) in enumerate(sign_dict):
-        save_dict[key] = [idx + 1, value]
-    np.save(f"./{args.dataset}/gloss_dict.npy", save_dict)
+
